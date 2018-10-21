71df4a5ce30102ed9b1b76cc309c27034b2804b0
Liliana Munoz <lilimunoz30@gmail.com>
2018-10-21 14:08:31 -0500

    Book Review Comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         def full_name
+         	return 'Anonymous' unless (first_name && last_name)
+		first_name + " " + last_name 
+	end
+
+	def initials
+		return 'AN' unless (first_name && last_name)
+		first_name.try(:first) + "." + last_name.try(:first) + "." 
+	end
 end
